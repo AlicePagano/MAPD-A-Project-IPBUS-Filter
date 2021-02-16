1f6bfc06fa971ca876ab96609332ec3ee06bdfc4
“AlicePagano” <alice.pagano@studenti.unipd.it>
2021-02-16 13:59:28 +0100

    final

--- src/software/ipbus_test.py
+++
@@ -1,8 +0,0 @@
-import uhal
-
-if __name__ == "__main__":
-
-   # Code for the connection with the board
-
-   manager = uhal.ConnectionManager("file://arty7_connection.xml")
-   hw = manager.getDevice("arty7")
